56ada515c22e704e09ce777926fbb8ce66958731
MHindermann <maximilian.hindermann@gmail.com>
2025-02-26 23:04:20 +0100

    add images and completions

---
+++ benchmarks/metadata_extraction/person.py
@@ -0,0 +1,36 @@
+""" person.py
+============
+Person class. """
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Person:
+    """Representation of a person.
+
+    :param name: name of the person
+    :param inferred_from_function: whether the person was inferred from a job function
+    :param inferred_from_correspondence: whether the person was inferred from the correspondence history
+    """
+
+    name: str
+    inferred_from_function: bool = False
+    inferred_from_correspondence: bool = False
+
+    @staticmethod
+    def from_string(person: str) -> "Person":
+        """Create a Person object from a string.
+
+        Used to parse persons in persons.csv exported from Google Sheets. Inferred attributes are set to True given
+        the angle brackets conventions.
+
+        :param person: string representation of a person (i.e., their name)
+        """
+
+        person = person.strip()
+        if person.startswith("<<") and person.endswith(">>"):
+            return Person(name=person[2:-2].strip(), inferred_from_correspondence=True)
+        elif person.startswith("<") and person.endswith(">"):
+            return Person(name=person[1:-1].strip(), inferred_from_function=True)
+        return Person(name=person)
